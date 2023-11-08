6597c4a60c6b431185d06ca10720686c3a820dc1
Chetti <nagarjuna-kumar.chetti@capgemini.com>
2023-11-08 13:11:57 +0530

    user service

---
+++ src/main/java/com/osm/in/service/UserService.java
@@ -0,0 +1,18 @@
+package com.osm.in.service;
+
+import java.util.List;
+
+import com.osm.in.entity.User;
+
+
+
+public interface UserService {
+	
+	public User addUser(User user);
+	
+	public User updateUser(User user);
+	
+	public User cancelUser(long userId);
+	
+	public List<User> showAllUsers();
+}
--- src/main/java/com/osm/in/service/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.osm.in.service;
