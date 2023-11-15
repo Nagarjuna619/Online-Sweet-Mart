1a822d4f7289f4d1a8d2791061af5fc81f2ddbbb
allapkum8 <alla-phanindra.kumar@capgemini.com>
2023-11-15 10:07:14 +0530

    done

--- src/main/java/com/osm/in/entity/Product.java
+++ src/main/java/com/osm/in/entity/Product.java
@@ -1,7 +1,5 @@
 package com.osm.in.entity;
 
-import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,10 +7,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
+
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.lang.Nullable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,27 +30,48 @@ import lombok.NoArgsConstructor;
 @Builder
 @Table(name = "product")
 public class Product {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer productId;
+	
+	
+	
+	@Size(min = 3, max=10,message="length should be 3")
 	private String name;
+	
+	@NotBlank
+	//@Length(min =3,max = 100)
 	private String photoPath;
+	
+	@NotNull
 	private Double price;
+	
+	@NotNull
+	//@Length(min =3,max = 100)
 	private String description;
 	
-	@Builder.Default
+	@NotNull
+	
 	private Boolean available = true;
 	
 	
-	@ManyToOne(cascade = CascadeType.REMOVE)
+	@ManyToOne(cascade = CascadeType.ALL)
+	
 	@JoinColumn(name = "cart_id")
+	
+	@JsonIgnore
 	private Cart cart;
 	
+	
+
+	//@Length(min =3,max = 100)
 	@ManyToOne
+
+	@JsonIgnore
 	private Category category;
 	
-	@OneToMany(cascade = CascadeType.ALL)
-	private List<SweetItem> itemList;
+	
 	
 }
 
